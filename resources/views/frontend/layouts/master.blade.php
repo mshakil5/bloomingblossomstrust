162387e7eb8d535bc7a4b338f3fc53662ea41e5b
shakil <46122677+mshakil5@users.noreply.github.com>
2025-11-15 11:43:27 +0600

    meta title

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -11,7 +11,6 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>{{ $company->meta_title ?? $company->company_name }}</title>
         {!! SEOMeta::generate() !!}
         {!! OpenGraph::generate() !!}
         {!! Twitter::generate() !!}
